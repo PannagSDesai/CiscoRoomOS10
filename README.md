b9964ab600c51fbe24b191b6b1f7783625e2067b
Pannag <dsbihi123@gmail.com>
2022-07-25 06:03:50 +0530

    Adding Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is Cisco Room OS 10 SDK which adds abstraction to the xAPI allowing devlopers-administrators to use most xAPI functions with Python
+without having to worry about parsing the results.
+It also adds extra functionalities to test configuration.
--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from setuptools import setup
 
 setup(
    name='CiscoRoomOS10',
-   version='0.0.3',
+   version='0.0.1',
    license="MIT",
    description='A library written for python to provide abstraction for Cisco xAPI which is used in administration of Video Conferencing devices running RoomOS10',
    author='Pannag Desai',
-   url="https://github.com/PannagSDesai/RomanLibrary.git",
-   download_url="https://github.com/PannagSDesai/EasyRoman/archive/refs/tags/Numbers.tar.gz",
+   url="https://github.com/PannagSDesai/CiscoRoomOS10",
+   download_url="https://github.com/PannagSDesai/CiscoRoomOS10/archive/refs/tags/Cico_Video_SDK.tar.gz",
    keywords=["Number system conversion"],
    long_description="This library provides most functions applicable for all models",
    author_email='pannag.desai@outlook.com',
